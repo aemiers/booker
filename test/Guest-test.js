d129d80ec4d02a83ada0d0cf950d243881c83fb4
Anneke Miers <annekemiers.am@gmail.com>
2021-03-04 20:13:55 -0700

    Add base to test

---
+++ test/Guest-test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+const expect = chai.expect;
+
+describe('Guest', () => {
+  let guest;
+
+  beforeEach(() => {
+  })
+}
---
+++ test/Hotel-test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+const expect = chai.expect;
+
+describe('Hotel', () => {
+  let guest, hotel;
+
+  beforeEach(() => {
+  })
+}

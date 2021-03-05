58884626029e222bdfc5f77cdcb86c83284d6670
Anneke Miers <annekemiers.am@gmail.com>
2021-03-04 21:42:11 -0700

    Add base for guest class and test

--- test/Guest-test.js
+++ test/Guest-test.js
@@ -1,9 +1,51 @@
 import { expect } from 'chai';
-const expect = chai.expect;
+import Guest from '../src/Guest';
 
 describe('Guest', () => {
-  let guest;
 
-  beforeEach(() => {
-  })
-}
+  const userData = {
+    "customers": [
+      {
+        "id": 1,
+        "name": "Leatha Ullrich"
+      },
+      {
+        "id": 2,
+        "name": "Rocio Schuster"
+      },
+    ]
+  }
+  const guestOne = new Guest(userData.customers[0]);
+  const guestTwo = new Guest(userData.customers[1])
+
+  describe('Properties', () => {
+    it('should be a function', () => {
+      expect(Guest).to.be.a('function');
+    });
+
+    it('should be an instance of Guest', () => {
+      expect(guestOne).to.be.an.instanceof(Guest);
+    });
+
+    it('should have an id and a name', () => {
+      expect(guestOne.id).to.equal(1);
+      expect(guestOne.name).to.equal("Leatha Ullrich");
+      expect(guestTwo.id).to.equal(2);
+      expect(guestTwo.name).to.equal("Rocio Schuster");
+
+    });
+
+    it('should start off with no bookings or future bookings', () => {
+      expect(guestOne.previousBookings.length).to.equal(0);
+      expect(guestOne.futureBookings.length).to.equal(0);
+    })
+
+  });
+
+  describe('Methods', () => {
+    it('should return a guests first name', () => {
+      expect(guestOne.getFirstName()).to.equal("Leatha");
+      expect(guestTwo.getFirstName()).to.equal("Rocio");
+    });
+  });
+});
--- test/Hotel-test.js
+++ test/Hotel-test.js
@@ -1,9 +1,43 @@
-import { expect } from 'chai';
-const expect = chai.expect;
+// import { expect } from 'chai';
+// import Guest from '../src/Guest';
+// import Hotel from '../src/Hotel';
 
-describe('Hotel', () => {
-  let guest, hotel;
+// const expect = chai.expect;
 
-  beforeEach(() => {
-  })
-}
+// describe('Hotel', () => {
+//   let userData, guest, hotel;
+
+// beforeEach(() => {
+//   userData = {
+//     "customers": [
+//       {
+//         "id": 1,
+//         "name": "Leatha Ullrich"
+//       },
+//       {
+//         "id": 2,
+//         "name": "Rocio Schuster"
+//       },
+//     ]
+//   }
+
+//   user1 = new User(userData);
+//   user2 = new User(userData)
+// })
+
+// describe('Properties', () => {
+//   it('should be a function', () => {
+//     expect(Hotel).to.be.a('function');
+//   });
+
+//   //   it('should be an instance of Hydration', () => {
+//   //     expect(user1).to.be.an.instanceof(Guest);
+//   //   });
+
+//   //   it('should take in a list of data', () => {
+//   //     expect(user1.dataSet[0].userID).to.equal(1);
+//   //     expect(user1.dataSet[2].numOunces).to.equal(30);
+//   //     expect(user1.dataSet[4].date).to.equal("2018/10/23");
+//   //   });
+// });
+// })

34d5754b8c73bb48c11645fe5f26b3bcbbfc4bd5
Anneke Miers <annekemiers.am@gmail.com>
2021-03-07 08:19:29 -0700

    Fix search bar positioning and format

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-import './css/base.scss';
+// import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
+import './css/index.scss';
+import './images/bed.svg';
+import './images/bedroom1.jpg';
+import './images/bedroom2.jpg';
+import './images/bedroom3.jpg';
+import './images/bedroom4.jpg';
+import './images/dollar-symbol.svg';
+import './images/logo.png';
+import './images/neuschwanstein.jpg';
+import './images/toilet.svg';
+import './images/search.svg';
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+// console.log('This is the JavaScript entry file - your code begins here.');
+import Customer from './Customer';
+import Hotel from './Hotel';
